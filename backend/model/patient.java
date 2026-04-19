15c6f53039a9f3ec2385ddbd161ff11f13a24eb5
fmalinao-rgb <fmalinao@usa.edu.ph>
2026-04-19 11:45:55 +0800

    Patient.java

--- backend/model/patient.java
+++ backend/model/patient.java
@@ -1,1 +1,44 @@
 #Paadd code
+
+import java.util.*;
+
+public class Patient {
+    private Long id;
+    private String name;
+    private int age;
+    private String address;
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Patient p = new Patient();
+
+        System.out.print("Name: ");
+        p.setName(sc.nextLine());
+
+        System.out.print("Age: ");
+        p.setAge(sc.nextInt());
+        sc.nextLine();
+
+        System.out.print("Address: ");
+        p.setAddress(sc.nextLine());
+
+        System.out.println("\n--- Patient Info ---");
+        System.out.println("Name: " + p.getName());
+        System.out.println("Age: " + p.getAge());
+        System.out.println("Address: " + p.getAddress());
+
+        sc.close();
+    }
+}
